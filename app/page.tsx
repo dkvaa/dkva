fbb88d746066f4ce6a68faac8bb48454750c01a5
Diva Aprillia <163496018+dkvaa@users.noreply.github.com>
2024-10-21 16:29:35 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import ContactForm from "./components/contactForm";
+import FavColor from "./components/favColor";
+import Hero from "./components/hero";
+import RiwayatPekerjaan from "./components/riwayatPekerjaan";
+import RiwayatPendidikan from "./components/riwayatPendidikan";
+import "./diva-styles.css";
+
+
+export default function CvOnline() {
+  return (
+    <section>
+    <Hero />
+    <RiwayatPendidikan />
+    <RiwayatPekerjaan />
+    <FavColor/>
+    <ContactForm/>
+    </section>
+  );
+}
+
+
